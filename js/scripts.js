b3925edd060f67a99de7f10f303e1d5df1f493f5
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-16 19:50:59 -0700

    work with basic requirement

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+//alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+var message = "";
+
+$("#play").click(function() {
+  var yourNumber = parseInt($("#myNumber").val());
+  for (i=1; i<=yourNumber; i++) {
+    if (i%15===0) {
+      message = "ping-pong";
+    } else if (i%5===0) {
+      message = "pong";
+    } else if (i%3===0) {
+      message = "ping";
+    } else {
+      message = i;
+    }
+    $("#pingpong").append("<li><strong>" + message + "</strong></li>");
+  }
+
+});
