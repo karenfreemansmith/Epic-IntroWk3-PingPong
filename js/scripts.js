74556071dfb4a6c5bbfe89186cb747d574a65bab
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-08-19 08:17:42 -0700

    update bootstrap/jquery, separate ui and business logic, pass first test

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
-//alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
-var message = "";
-
-$("#play").click(function() {
-  var yourNumber = parseInt($("#myNumber").val());
-  for (i=1; i<=yourNumber; i++) {
-    if (i%15===0) {
-      message = "ping-pong";
-    } else if (i%5===0) {
-      message = "pong";
-    } else if (i%3===0) {
-      message = "ping";
-    } else {
-      message = i;
-    }
-    $("#pingpong").append("<li><strong>" + message + "</strong></li>");
-  }
-
+$(document).ready(function() {
+  $("form#play").submit(function(event) {
+    event.preventDefault();
+    $("#pingpong").append("<p>" + playpong(parseInt($("#userNumber").val())) + "</p>");
+  });
 });
+
+function playpong(userNumber) {
+ var message="";
+ for (i=1; i<=userNumber; i++) {
+ //     if (i%15===0) {
+ //       message = "ping-pong";
+ //     } else if (i%5===0) {
+ //       message = "pong";
+ //     } else if (i%3===0) {
+ //       message = "ping";
+ //     } else {
+       message = i;
+ //     }
+    return message;
+   }
+ }
