44b653aba99faec1403bd24a77108633ea586b06
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-08-19 08:27:15 -0700

    print ping if number is divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,18 @@ $(document).ready(function() {
 });
 
 function playpong(userNumber) {
- var message=[];
- for (i=1; i<=userNumber; i++) {
- //     if (i%15===0) {
- //       message = "ping-pong";
- //     } else if (i%5===0) {
- //       message = "pong";
- //     } else if (i%3===0) {
- //       message = "ping";
- //     } else {
-       message.push(i);
- //     }
-
-   }
-   return message;
+  var message=[];
+  for (i=1; i<=userNumber; i++) {
+  //     if (i%15===0) {
+  //       message = "ping-pong";
+  //     } else if (i%5===0) {
+  //       message = "pong";
+  //     } else
+    if (i%3===0) {
+      message.push("ping");
+    } else {
+      message.push(i);
+    }
+  }
+  return message;
  }
